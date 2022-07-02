93a70abbe29d77e1b1d346e3412ac7a07e45bbbd
Thuta Min Thway <thutaminthway@Thutas-MacBook-Air.local>
2022-07-02 19:35:38 +0630

    buffer and readable stream

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-        data: 'Hello World!'
-    }));
-});
+var readMyStream = fs.createReadStream(__dirname + "/readme.txt", "utf8");
 
-server.listen(3000, '127.0.0.1');
-console.log('now listening to port 3000');
+readMyStream.on("data", (chunk) => {
+    console.log("new chunk received");
+    console.log(chunk);
+})
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.end(JSON.stringify({
+//         data: 'Hello World!'
+//     }));
+// });
+//
+// server.listen(3000, '127.0.0.1');
+// console.log('now listening to port 3000');
