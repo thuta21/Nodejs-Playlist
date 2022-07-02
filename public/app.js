b6c3ab364bf040e574089be46ea799d52d4290b7
Thuta Min Thway <thutaminthway@Thutas-MacBook-Air.local>
2022-07-02 11:55:49 +0630

    file system read and write

--- public/app.js
+++ public/app.js
@@ -1,26 +1,7 @@
-const events = require("node:events");
-const util = require('util');
-var Person = function(name) {
-    this.name = name;
-}
+const fs = require("fs");
 
-util.inherits(Person, events)
-
-// class Person extends events.EventEmitter{
-//     constructor(name){
-//         super();
-//         this.name = name;
-//     }
-// }
-
-var thuta = new Person("thuta");
-var lara = new Person("lara");
-
-var people = [thuta, lara];
-people.forEach((p) => {
-    p.on("speak", function ($message) {
-        console.log(p.name + ' said ' + $message)
-    })
+fs.readFile("readme.txt", "utf8", function (err, data){
+    fs.writeFile("write.txt",data, () => {
+        console.log('success');
+    });
 })
-
-thuta.emit("speak", "Hello Motherfucker")
