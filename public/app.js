d17fc280eb833a9a48a8c77bf2f9e856f216a5fc
Thuta Min Thway <thutaminthway@Thutas-MacBook-Air.local>
2022-07-02 11:25:49 +0630

    events and util

--- public/app.js
+++ public/app.js
@@ -1,3 +1,26 @@
-let stuff = require('./count');
-// console.log(counter(["apple", "orange", "banana"]));
-console.log(stuff.add(1, 1))
+const events = require("node:events");
+const util = require('util');
+var Person = function(name) {
+    this.name = name;
+}
+
+util.inherits(Person, events)
+
+// class Person extends events.EventEmitter{
+//     constructor(name){
+//         super();
+//         this.name = name;
+//     }
+// }
+
+var thuta = new Person("thuta");
+var lara = new Person("lara");
+
+var people = [thuta, lara];
+people.forEach((p) => {
+    p.on("speak", function ($message) {
+        console.log(p.name + ' said ' + $message)
+    })
+})
+
+thuta.emit("speak", "Hello Motherfucker")
